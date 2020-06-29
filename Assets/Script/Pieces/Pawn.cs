248a7b0c96773896729d15cb35fdf5d184f94137
chauhuynhduc99 <61837023+chauhuynhduc99@users.noreply.github.com>
2020-06-29 16:29:05 +0700

    Update Pawn.cs

--- Assets/Script/Pieces/Pawn.cs
+++ Assets/Script/Pieces/Pawn.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
 
 public class Pawn : BasePiece
 {
